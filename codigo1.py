ae1e4e30e7d47d6969cab16a7bda109d7e8166f2
Ana Luiza Botelho <aluiza.primo@gmail.com>
2024-11-10 20:09:34 -0300

    criando lista de estudante

--- codigo1.py
+++ codigo1.py
@@ -3,3 +3,10 @@ class Estudante:
         self.nome = nome
         self.email = email
 
+lista = []
+
+def adicionar_lista(Estudante):
+    lista.append(Estudante)
+    print("Estudante cadastrado com sucesso!")
+
+
